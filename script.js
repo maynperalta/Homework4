68f2b8d8ecbb1d8c4bc02d76b678c0982727530d
Maynard Peralta <maynperalta@gmail.com>
2020-07-19 12:09:30 -0600

    added form info

--- script.js
+++ script.js
@@ -6,7 +6,6 @@
 // create a JavaScript file which will have all of our script in order to give feedback on the answers.
 //     feedback will involve whether the answer is right or wrong.
 //     answer will change the question to the next question
-//     right answer will add time to the timer
 //     wrong answer will subtract time from the timer.
 //     when timer reaches zero, quiz is over.
 //     when all questions have been answered, quiz is over.
@@ -21,23 +20,51 @@ const questionContainerElement = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButtonsElement = document.getElementById("answer-buttons");
 const button = document.querySelector("button");
-// const hiScore = getElementById("scores")
+const hiScore = socument.querySelector("#scores");
 let shuffledQuestions, currentQuestionIndex
-var answerBoolean 
+var recordButton = document.getElementById("record");
+var msgDiv = document.querySelector("#msg");
+var initialInput = document.querySelector("#initials");
+var initialInputSpan = document.getElementById("#user-initials");
 var score = 0
 
-// hiScore.addEventListener("click", showScores)
+function displayMessage(type, message){
+  msgDiv.textContent = message;
+  msgDiv.setAttribute("class", type);
+}
 
-// function showScores(){
+recordButton.addEventListener("click",function(event){
+  event.preventDefault();
 
-// }
+  var user = {initial: initialInput.value.trim()};
+  
+  if (user.initial ===""){
+    displayMessage("error", "Please enter your initals");
+  }else{
+    displayMessage("Success!", "Score added.");
+  
+    console.log(user);
+    localStorage.setItem("user", user);
+
+    var lastUser = localStorage.getItem("user");
+      initialInputSpan.textContent = lastUser.initial;
+  }  
+});
+
+
+hiScore.addEventListener("click", showScores)
+
+function showScores(){
+  
+
+
+}
 
 startButton.addEventListener("click", startQuiz)
 nextButton.addEventListener("click", () => {
   currentQuestionIndex++
   setNextQuestion()
 })
-
 
 function startQuiz() {
   score = 0
@@ -48,10 +75,8 @@ function startQuiz() {
   } else {
 
   alert("Hello, " + name + ". Welcome to my quiz.") 
-  alert("You will be presented with five multiple choice questions regarding JavaScript.")
-  alert("Upon starting the quiz, you will have one minute to answer all of the questions.")
-  alert("Answering a question correctly will grant you one point.")
-  alert("Answering a question incorrectly will award zero points AND subtract five seconds off of the timer.")
+  alert("You will be presented with five multiple choice questions regarding JavaScript. Upon starting the quiz, you will have one minute to answer all of the questions.")
+  alert("Answering a question correctly will grant you one point. However, answering a question incorrectly will award zero points AND subtract five seconds off of the timer.")
   alert("If you manage to answer all 5 questions in the allotted time, your score will be multiplied by the time remaining in order to calculate your final score.")
   alert("Are you ready? Here we go!")}
 
@@ -61,13 +86,23 @@ function startQuiz() {
   questionContainerElement.classList.remove("hide")
   setNextQuestion()
   timer()
-    }
-
+   
+  var timeEl = document.getElementById("counter");
+  var secondsLeft = 60;
+    function timer(){
+      var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.innerHTML = secondsLeft;
+        if(secondsLeft === 0){
+          clearInterval(timerInterval);
+          alert("I'm sorry. Time is up. Please try again.")
+          return;
+        }}, 1000);}
+}
 
 function setNextQuestion() {
   resetState()
   showQuestion(shuffledQuestions[currentQuestionIndex])
-
 }
 
 function showQuestion(question) {
@@ -79,20 +114,13 @@ function showQuestion(question) {
       button.dataset.correct = answer.correct
     button.addEventListener("click", selectAnswer)
     answerButtonsElement.appendChild(button)
-  })
-}
-
-function checkAnswer(event) {
-  console.log(event)
-}
+  })}
 
 function resetState(){
   nextButton.classList.add("hide")
   while (answerButtonsElement.firstChild){
     answerButtonsElement.removeChild(answerButtonsElement.firstChild)
-  }
-}
-
+  }}
 
 function selectAnswer(e, answerBoolean) {
   const selectedButton = e.target
@@ -104,12 +132,11 @@ function selectAnswer(e, answerBoolean) {
   }else{
     startButton.innerText = "Retry"
     startButton.classList.remove("hide")
-
     alert("The quiz is complete. Your final score is: " + score + ". Please enter your initials to keep track of your score!")
-  }
-}
 
-function setStatusClass(element, answerBoolean) {
+  }}
+
+function setStatusClass(event, answerBoolean) {
   console.log("------------------------------------");
   console.log(answerBoolean)
   document.getElementById('answer-buttons').innerHTML = "";
@@ -122,75 +149,44 @@ function setStatusClass(element, answerBoolean) {
     document.getElementById('question').innerHTML = "Incorrect."
     alert("I'm sorry. That is incorrect.")
     console.log(score)
-  }
-}
+  }}
 
-var timeEl = document.querySelector(".counter");
-var secondsLeft = 60;
-  function timer(){
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.innerHTML = secondsLeft;
-      if(secondsLeft === 0){
-        clearInterval(timerInterval);
-        alert("I'm sorry. Time is up. Please try again.")
-      }
-    }, 1000);
-  }
-
-
-const question = [
-  {
-    question: "What does the abbreviation 'var' stand for?",
+const question = [{question: "What does the abbreviation 'var' stand for?",
     answers: [
       {text: "Variable", correct: true},
       {text: "Variety", correct: false},
       {text: "Various", correct: false},
       {text: "Varsity", correct: false}
-    ], 
-    correctAnswer: 0
+    ],},
 
-  },
-  {
-    question: "What is needed in order for a function to operate?",
+  {question: "What is needed in order for a function to operate?",
     answers: [
       {text: "Fuel", correct: false},
       {text: "Permission", correct: false},
       {text: "Call", correct: true},
       {text: "Button", correct: false}
-    ],
-    correctAnswer: 2
-  },
-  {
-    question: "How do I add to an array?",
+    ],},
+
+  {question: "How do I add to an array?",
     answers: [
       {text: "Push", correct: false},
       {text: "Concat", correct: true},
       {text: "Add", correct: false},
       {text: "Combine", correct: false}
-    ],
-    correctAnswer: 1
-  },
+    ],},
 
-  {
-    question: "What tag would designate JavaScript data?",
+  {question: "What tag would designate JavaScript data?",
     answers: [
       {text: "Head", correct: false},
       {text: "Style", correct: false},
       {text: "Script", correct: true},
       {text: "Meta", correct: false}
-    ],
-    correctAnswer: 2
-  },
+    ],},
 
-  {
-    question: "What event listener involves a mouse?",
+  {question: "What event listener involves a mouse?",
     answers: [
       {text: "Change", correct: false},
       {text: "Keypress", correct: false},
       {text: "Submit", correct: false},
       {text: "Click", correct: true}
-    ],
-    correctAnswer: 3
-  },
-]
+    ],},]
